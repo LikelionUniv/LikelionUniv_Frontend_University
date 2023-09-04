c8241575572d6b28f5d390a3937b473c8ddc9857
harim061 <doris5093@duksung.ac.kr>
2023-09-04 11:29:37 +0900

    [Feat] recruit 페이지 헤더 제작

---
+++ src/components/recruit/ContentStyle.tsx
@@ -0,0 +1,26 @@
+import { styled } from 'styled-components';
+
+export const ContentDiv = styled.div`
+    padding-top: 160px;
+
+    color: #f2f4f6;
+    padding-left: 360px;
+`;
+
+export const Title = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    :nth-child(2) {
+        font-size: 16px;
+        margin-left: 16px;
+    }
+    margin-bottom: 40px;
+`;
+
+export const TD = styled.div`
+    color: #212224;
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 700;
+`;
