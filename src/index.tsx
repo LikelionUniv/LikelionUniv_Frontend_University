548e628089beab20158255b1f03ff296798525ff
byeong <qudcks4885@gmail.com>
2023-09-12 16:06:42 +0900

    병찬 : styled-reset 설치 , 글로벌 스타일 초기화

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './Router';
+import GlobalStyles from './styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
 );
 root.render(
     <React.StrictMode>
+        <GlobalStyles/>
         <RouterProvider router={router}></RouterProvider>
     </React.StrictMode>,
 );
