04546c2391972ce1cb145b07cb1dbaa8fa488800
Ariling <97501226+Ariling@users.noreply.github.com>
2023-09-21 14:56:56 +0900

    feat : 프로젝트 파트 제외 카드 구현

--- src/components/mypage/PostCardWithPhoto.tsx
+++ src/components/mypage/PostCardWithPhoto.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import { PostCardBox, PostCardBoxWrapper } from './PostCard';
+import { PostCardProp } from './type';
 
-const PostCardWithPhoto = () => {
-    return <div>PostCardWithPhoto</div>;
+const PostCardWithPhoto = (props: PostCardProp) => {
+    return (
+        <PostCardBoxWrapper className="photo">
+            <PostCardBox
+                className="photo"
+                style={{
+                    backgroundImage: `url(${props.img})`,
+                }}
+            ></PostCardBox>
+            <PostCardBox className="date">{props.date}</PostCardBox>
+            <PostCardBox className="title">{props.title}</PostCardBox>
+            <PostCardBox className="photocontent">{props.content}</PostCardBox>
+            <PostCardBox className="nav">
+                <div className="wrapper">
+                    <div className="heart" />
+                    <div>{props.like}</div>
+                </div>
+                <div className="wrapper">
+                    <div className="comment" />
+                    <div>{props.comment}</div>
+                </div>
+            </PostCardBox>
+        </PostCardBoxWrapper>
+    );
 };
 
 export default PostCardWithPhoto;
