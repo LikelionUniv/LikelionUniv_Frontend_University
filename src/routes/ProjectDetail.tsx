211abe0689e332da0cb62a92f8f230bae0f30d05
zizonyoungjun <naya700@naver.com>
2023-09-27 22:34:30 +0900

    feat: project detail router

---
+++ src/routes/ProjectDetail.tsx
@@ -0,0 +1,12 @@
+import ProjectDetail from "../components/project/Detail/ProjectDetail";
+
+const Project = () => {
+    return (
+        <div className="pDiv">
+            <h1>프로젝트</h1>
+            <ProjectDetail />
+        </div>
+    );
+};
+
+export default Project;
