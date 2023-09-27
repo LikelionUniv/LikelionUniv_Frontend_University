6c285f2b8324c53d324494b6f9a6d4b4fbafe5b6
zizonyoungjun <naya700@naver.com>
2023-09-27 22:34:11 +0900

    feat: svg 추가

---
+++ src/img/project/detail/index.ts
@@ -0,0 +1,6 @@
+export { default as LeftArrow } from './icon-leftArrow.svg';
+export { default as RightArrow } from './icon-rightArrow.svg';
+export { default as SwiperLeft } from './icon-swiperLeft.svg';
+export { default as SwiperRight } from './icon-swiperRight.svg';
+export { default as PopModal } from './icon-PopModal.svg';
+//export { default as ProjectImg } from './project-detail.png';
