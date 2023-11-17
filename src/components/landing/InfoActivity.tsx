a585d796d80dcc4e19a9773d948fa2a3e8e271a7
yanghwayeong <hwayeong@yanghwayeongui-MacBookPro.local>
2023-11-17 13:08:42 +0900

    .

--- src/components/landing/InfoActivity.tsx
+++ src/components/landing/InfoActivity.tsx
@@ -1,85 +1,117 @@
-import { useState } from 'react';
 import * as IF from './Information.style';
-import HackathonImg from '../../img/landing/hackathon.png';
-import IdeathonImg from '../../img/landing/ideathon.png';
-import StudyImg from '../../img/landing/study.png';
-import DemoImg from '../../img/landing/demo.png';
-
 import { ReactComponent as Arrow } from '../../img/landing/pixel_arrow_white.svg';
+import frontEndImage from '../../img/landing/frontend.png';
+import backEndImage from '../../img/landing/back.png';
+import designImage from '../../img/landing/design.png';
+import planningImage from '../../img/landing/plan.png';
+import frontEndImageMobile from '../../img/landing/frontEndImageMobile.png'
+import backEndImageMobile from '../../img/landing/BackEndImageMobile.png'
+import planningImageMobile from '../../img/landing/planingImageMobile.png'
+import DesignImageMobile from '../../img/landing/DesignImageMobile.png'
+import frontEndsmallImageMobile from '../../img/landing/frontEndsmallImageMobile.png'
+import backEndsmallImageMobile from '../../img/landing/backEndsmallImageMobile .png'
+import planningsmallImageMobile from '../../img/landing/planningsmallImageMobile .png'
+import DesignsmallImageMobile from '../../img/landing/DesignsmallImageMobile.png'
 
-
-export interface BoxProps {
-    background?: string;
-    hoverBackColor?: string;
+export interface TrackBoxProps {
+  backgroundColor?: string;
+  hoverColor?: string;
 }
 
-const activityArray: any[] = [
-    {
-        background:'var(--Grey-900, #212224)', 
-        hoverBackColor:  
-            'linear-gradient(180deg, rgba(174, 180, 187, 0.30) 0%, rgba(174, 180, 187, 0.00) 100%)' ,
-        name: '해커톤',
-        text: '멋대의 꽃, 해커톤은 무박 2일 동안 전체 인원이 한데 모여 특정 주제에 맞는 서비스를 구현해 내는 행사입니다',
-        color:'var(--Grey-600, #ADB3BA)',
-
-
-    },
-    {
-        background:'var(--Grey-900, #212224)', 
-        hoverBackColor:
-            'linear-gradient(180deg, rgba(255, 119, 16, 0.20) 0%, rgba(255, 119, 16, 0.00) 100%)',
-        name: '아이디어톤',
-        text: '활동 시작 이후 가장 처음 진행되는 전체 행사로, 전국 멋대 아기사자들의 창의적인 아이디어를 가감 없이 볼 수 있는 행사입니다.',
-        color:'var(--Orange-600, #FF7710)',
-    },
-    {
-        background:'var(--Grey-900, #212224)', 
-        hoverBackColor:
-            'linear-gradient(180deg, rgba(198, 249, 89, 0.20) 0%, rgba(198, 249, 89, 0.00) 100%)',
-        name: '학교별 스터디',
-        text: '활동 기간 동안 각 학교에서 트랙별 스터디를 진행합니다. 단순히 특정 직무 지식을 쌓는 기간이 아닌, 내 아이디어를 현실화할 수 있는 무기들을 갈고 닭는 시간이라 생각하며 자율 스터디에 참여해보세요. ',
-        color:'var(--Lime-600, #C6F959)',
-
-
-    },
-    {
-        background:'var(--Grey-900, #212224)', 
-        hoverBackColor:
-            'linear-gradient(180deg, rgba(255, 177, 60, 0.20) 0%, rgba(255, 177, 60, 0.00) 100%)',
-        name: '데모데이',
-        text: '1년 동안 열심히 활동하며 만들어 낸 우리 서비스가 투자, M&A 등을 통해 실제 창업으로 이어질 수 있는 단계입니다. POSSIBILITY TO REALITY! 멋쟁이사자처럼 대학은 아이디어를 실현하는 모든 단계에서, 끝까지, 아기사자 여러분을 지원합니다.   ',
-        color:'var(--Tangerine-600, #FFB13C)',
-
-
-    },
+const trackArray: any[] = [
+  {
+    hoverColor: "#212224",
+    initialColor: "#FFB13C",
+    name: "프론트엔드",
+    text: "사용자와 가까이 있는 개발 파트로, 웹 클라이언트 개발을 위한 기초부터 심화까지의 스킬을 배울 수 있습니다. HTML, CSS, Javascript를 학습하며 기초 개발 역량을 잡아나갑니다.",
+    image: frontEndImage,
+    imageMobile: frontEndImageMobile,
+    imageSmallMobile: frontEndsmallImageMobile,
+  },
+  {
+    hoverColor: "#212224",
+    initialColor: "#FF7710",
+    name: "백엔드",
+    text: "눈에 보이지 않는 서버를 전반적으로 담당하는 백엔드는, 서비스의 요구사항에 맞춰 API를 개발하고 서비스 배포 및 운영을 통해 데이터를 관리합니다. Django, DRF, Spring 등 다양한 프레임워크를 바탕으로 서비스 운영을 위한 전체적인 인프라를 구현합니다.",
+    image: backEndImage,
+    imageMobile: backEndImageMobile,
+    imageSmallMobile: backEndsmallImageMobile,
+  },
+  
+  {
+    hoverColor: "#212224",
+    initialColor: "#C6F959",
+    name: "디자인",
+    text: "디자인 툴을 익히고 다양한 웹 디자인 이론과 용어들을 배우며 협업에 도움이 되는 스킬을 키울 수 있습니다. 이를 통해 협업에 필요한 내용들을 설계하는 경험을 할 수 있습니다.",
+    image: designImage,
+    imageMobile: DesignImageMobile,
+    imageSmallMobile: DesignsmallImageMobile, 
+  },
+  {
+    hoverColor: "#212224",
+    initialColor: "#ADB3BA",
+    name: "기획",
+    text: "Team Leader로서 프로젝트의 진행을 리드하며 서비스 기획자, PM이 되기 위한 역량을 기를 수 있습니다. 이를 통해 협업에 필요한 내용들을 설계하는 경험을 할 수 있습니다.",
+    image: planningImage,
+    imageMobile: planningImageMobile,
+    imageSmallMobile: planningsmallImageMobile,
+  
+  },
 ];
 
-const InfoActivity = () => {
-    return (
-        <div style={{ width: '100%' }}>
-            <IF.ActivityContainer>
-                {activityArray.map(item => (
-                    <IF.Box
-                        background={item.background}
-                        hoverBackColor={item.hoverBackColor}
-                    >
-                        <div className="title">
-                        <div className="name" style={{ color: item.color }}>{item.name}</div>
-                            <Arrow className="hover-hide" />
-                        </div>
-                        <div className='img-wrapper'>
-                            {item.name === '해커톤' && <img src={HackathonImg} className="img-hide" alt="" />}
-                            {item.name === '아이디어톤' && <img src={IdeathonImg} className="img-hide" alt="" />}
-                            {item.name === '학교별 스터디' && <img src={StudyImg} className="img-hide" alt="" />}
-                            {item.name === '데모데이' && <img src={DemoImg} className="img-hide" alt="" />}
-                        </div>
-                        
-                        <div className="hover-text">{item.text}</div>
-                    </IF.Box>
-                ))}
-            </IF.ActivityContainer>
-        </div>
-    );
+const InfoTrack = () => {
+  const isLargeScreen = window.innerWidth > 1024;
+  const isMediumScreen = window.innerWidth <= 1024 && window.innerWidth > 768;
+  const isSmallScreen = window.innerWidth <= 768;
+
+  const handleMouseEnter = (index: number) => {
+    const trackBox = document.getElementById(`trackBox-${index}`);
+    if (trackBox) {
+      trackBox.style.color = trackArray[index].initialColor;
+    }
+  };
+
+  const handleMouseLeave = (index: number) => {
+    const trackBox = document.getElementById(`trackBox-${index}`);
+    if (trackBox) {
+      trackBox.style.color = '';
+    }
+  };
+
+  return (
+    <IF.TrackWrapper>
+      {trackArray.map((item, index) => (
+        <IF.TrackBox
+          key={index}
+          backgroundColor={item.initialColor}
+          hoverColor={item.hoverColor}
+          onMouseEnter={() => handleMouseEnter(index)}
+          onMouseLeave={() => handleMouseLeave(index)}
+        >
+          <div className="title">
+            <div className="name" id={`trackBox-${index}`}>
+              {item.name}
+            </div>
+            <Arrow className="hover-hide" />
+          </div>
+          <div className="hover-text">{item.text}</div>
+          <div className="img-wrapper">
+            <img
+              src={
+                isSmallScreen
+                  ? item.imageSmallMobile
+                  : isMediumScreen
+                  ? item.imageMobile
+                  : item.image
+              }
+              alt=""
+              className="img-hide"
+            />
+          </div>
+        </IF.TrackBox>
+      ))}
+    </IF.TrackWrapper>
+  );
 };
 
-export default InfoActivity;
+export default InfoTrack;
--- src/components/landing/Information.style.ts
+++ src/components/landing/Information.style.ts
@@ -3,570 +3,267 @@ import { TrackBoxProps } from './InfoTrack';
 import { BoxProps } from './InfoActivity';
 
 export const Outer = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: #f2f4f6;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f2f4f6;
 `;
 
 export const Wrapper = styled.div`
-    width: calc(100% - 80px);
-    padding: 160px 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    @media (max-width: 1280px) {
-        height: auto;
-        padding: 140px 0;
-    }
-    @media (max-width: 768px) {
-        width: 100%;
-        flex-direction: column-reverse;
-        padding: 80px 0;
-    }
+  width: calc(100% - 80px);
+  padding: 160px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  @media (max-width: 1280px) {
+    height: auto;
+    padding: 140px 0;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+    flex-direction: column-reverse;
+    padding: 80px 0;
+  }
 
-    .container {
-        @media (max-width: 768px) {
-            width: calc(100% - 40px) !important;
-            max-width: 520px;
-            margin-left: 0;
-        }
+  .container {
+    @media (max-width: 768px) {
+      width: calc(100% - 40px) !important;
+      max-width: 520px;
+      margin-left: 0;
     }
+  }
 `;
 
 export const Title = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  white-space: nowrap;
+  color: #212224;
+  font-family: Pretendard;
+  font-weight: 700;
+  margin-bottom: 10px;
+  font-size: 48px;
+  height: 48px;
+  &.br {
     display: flex;
-    justify-content: flex-start;
     align-items: center;
-    white-space: nowrap;
-    color: #212224;
-    font-family: Pretendard;
-    font-weight: 700;
-    margin-bottom: 10px;
-    font-size: 48px;
-    height: 48px;
+    span {
+      display: flex;
+      align-items: center;
+      svg {
+        margin: 0 10px;
+      }
+    }
+  }
+  @media (max-width: 1280px) {
+    font-size: 3.6vw;
+    height: 3.6vw;
     &.br {
+      span {
+        height: 3.6vw;
         display: flex;
         align-items: center;
-        span {
-            display: flex;
-            align-items: center;
-            svg {
-                margin: 0 10px;
-            }
-        }
+      }
     }
+  }
+  @media (max-width: 768px) {
+    font-size: 28px;
+    height: 30px;
+    white-space: initial;
+    word-break: break-all;
+    &.br {
+      span {
+        height: 30px;
+        display: flex;
+        align-items: center;
+        white-space: nowrap;
+      }
+    }
+  }
+  @media (max-width: 480px) {
+    &.br {
+      height: auto;
+      flex-direction: column;
+      align-items: flex-start;
+      white-space: nowrap;
+      span {
+        height: 30px;
+        display: flex;
+        align-items: center;
+        margin-bottom: 4px;
+      }
+      .right {
+        margin-left: 0;
+      }
+    }
+  }
+
+  svg {
+    flex-shrink: 0;
+    margin: 0 8px;
+    height: 48px;
     @media (max-width: 1280px) {
-        font-size: 3.6vw;
-        height: 3.6vw;
-        &.br {
-            span {
-                height: 3.6vw;
-                display: flex;
-                align-items: center;
-            }
-        }
+      height: 100%;
+      width: auto;
+      margin: 0 1%;
     }
     @media (max-width: 768px) {
-        font-size: 28px;
-        height: 30px;
-        white-space: initial;
-        word-break: break-all;
-        &.br {
-            span {
-                height: 30px;
-                display: flex;
-                align-items: center;
-                white-space: nowrap;
-            }
-        }
+      margin: 0 5px;
     }
-    @media (max-width: 480px) {
-        &.br {
-            height: auto;
-            flex-direction: column;
-            align-items: flex-start;
-            white-space: nowrap;
-            span {
-                height: 30px;
-                display: flex;
-                align-items: center;
-                margin-bottom: 4px;
-            }
-            .right {
-                margin-left: 0;
-            }
-        }
-    }
-
-    svg {
-        flex-shrink: 0;
-        margin: 0 8px;
-        height: 48px;
-        @media (max-width: 1280px) {
-            height: 100%;
-            width: auto;
-            margin: 0 1%;
-        }
-        @media (max-width: 768px) {
-            margin: 0 5px;
-        }
-    }
+  }
 `;
 
 export const SubText = styled.div`
-    white-space: nowrap;
-    color: #212224;
-    font-family: Pretendard;
-    font-weight: 600;
-    font-size: 20px;
-    margin: 24px 0 40px 0;
-    @media (max-width: 768px) {
-        font-size: 16px;
-        white-space: initial;
-        word-break: keep-all;
-        line-height: 150%;
-        margin: 4% 0 8% 0;
-    }
+  white-space: nowrap;
+  color: #212224;
+  font-family: Pretendard;
+  font-weight: 600;
+  font-size: 20px;
+  margin: 24px 0 40px 0;
+  @media (max-width: 768px) {
+    font-size: 16px;
+    white-space: initial;
+    word-break: keep-all;
+    line-height: 150%;
+    margin: 4% 0 8% 0;
+  }
 `;
 
 export const SectionContainer = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    gap: 25px;
-    margin-bottom: 165px;
-    @media (max-width: 768px) {
-        flex-direction: column;
-    }
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 25px;
+  margin-bottom: 165px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 
-    .number-rect {
-        width: 100%;
-        margin-top: 5%;
-        .subtitle {
-            width: 100%;
-            color: #212224;
-            font-family: Pretendard;
-            font-weight: 700;
-            font-size: 24px;
-            padding-bottom: 10px;
-            border-bottom: 2px solid #212224;
-            @media (max-width: 1280px) {
-                font-size: 2vw;
-            }
-            @media (max-width: 768px) {
-                font-size: 20px;
-            }
-        }
-        .number {
-            margin-top: 16px;
-            display: flex;
-            justify-content: center;
-            color: #212224;
-            font-family: Pretendard;
-            font-weight: 700;
-            font-size: 72px;
-            sup {
-                font-size: 28px;
-                line-height: 200%;
-                margin-left: 5px;
-            }
-            @media (max-width: 1280px) {
-                font-size: 5.5vw;
-                sup {
-                    font-size: 2vw;
-                }
-            }
-            @media (max-width: 768px) {
-                font-size: 32px;
-                sup {
-                    font-size: 14px;
-                }
-            }
-        }
+  .number-rect {
+    width: 100%;
+    margin-top: 5%;
+    .subtitle {
+      width: 100%;
+      color: #212224;
+      font-family: Pretendard;
+      font-weight: 700;
+      font-size: 24px;
+      padding-bottom: 10px;
+      border-bottom: 2px solid #212224;
+      @media (max-width: 1280px) {
+        font-size: 2vw;
+      }
+      @media (max-width: 768px) {
+        font-size: 20px;
+      }
     }
+    .number {
+      margin-top: 16px;
+      display: flex;
+      justify-content: center;
+      color: #212224;
+      font-family: Pretendard;
+      font-weight: 700;
+      font-size: 72px;
+      sup {
+        font-size: 28px;
+        line-height: 200%;
+        margin-left: 5px;
+      }
+      @media (max-width: 1280px) {
+        font-size: 5.5vw;
+        sup {
+          font-size: 2vw;
+        }
+      }
+      @media (max-width: 768px) {
+        font-size: 32px;
+        sup {
+          font-size: 14px;
+        }
+      }
+    }
+  }
 `;
 
 export const SwiperWrapper = styled.div`
-    width: 100%;
-    overflow-x: hidden;
-    padding-bottom: 165px;
+  width: 100%;
+  overflow-x: hidden;
+  padding-bottom: 165px;
 
-    .swiper-container {
-        width: 984px !important;
-        @media (max-width: 1280px) {
-            width: calc((100vw - 36px) / 3 * 2 + 20px) !important;
-        }
-        @media (max-width: 768px) {
-            width: calc(100vw / 3 * 2 + 12px) !important;
-        }
-        display: flex;
-        overflow-x: visible;
-        margin: 0 auto;
+  .swiper-container {
+    width: 984px !important;
+    @media (max-width: 1280px) {
+      width: calc((100vw - 36px) / 3 * 2 + 20px) !important;
     }
-    .swiper-wrapper {
-        display: flex;
+    @media (max-width: 768px) {
+      width: calc(100vw / 3 * 2 + 12px) !important;
     }
-    .swiper-slide {
-        width: 480px !important;
-        @media (max-width: 1280px) {
-            width: calc((100vw - 36px) / 3) !important;
-        }
-        @media (max-width: 768px) {
-            width: 259px !important;
-        }
-        a {
-            text-decoration: none;
-        }
-        .rect {
-            width: 100%;
-            aspect-ratio: 48 / 20;
-            background-color: #fff;
-            border-radius: 8px;
-            display: flex;
-            align-items: center;
-        }
-        .logo-rect {
-            margin: 24px;
-            border-radius: 12px;
-            width: 31.25%;
-            aspect-ratio: 1 / 1;
-            overflow: hidden;
-            @media (max-width: 768px) {
-                width: 25%;
-                margin: 20px;
-            }
-            img {
-                width: 100%;
-                height: 100%;
-                object-fit: cover;
-                border-radius: 12px;
-            }
-        }
-        .text-section {
-            font-family: Pretendard;
-            color: #212224;
-            .name {
-                color: #212224;
-                font-size: 28px;
-                font-weight: 600;
-                line-height: 200%;
-                @media (max-width: 1280px) {
-                    font-size: 2.2vw;
-                }
-                @media (max-width: 768px) {
-                    font-size: 20px;
-                }
-            }
-            .gen {
-                font-size: 20px;
-                font-weight: 400;
-                @media (max-width: 1280px) {
-                    font-size: 1.6vw;
-                }
-                @media (max-width: 768px) {
-                    font-size: 15px;
-                }
-            }
-        }
-    }
-`;
-
-export const ActivityContainer = styled.div`
-    width: 100%;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-gap: 24px;
-
-    @media screen and (max-width: 767px) {
-        grid-template-columns: repeat(1, 1fr);
-    }
-`;
-
-export const Box = styled.div<BoxProps>`
     display: flex;
-    justify-content: space-between;
-    width: 100%;
-    box-sizing: border-box;
-    background: ${props => props.background || 'transparent'};
-    border-radius: 8px;
-    padding: 16px;
-
-    .hover-text {
-        display: none;
+    overflow-x: visible;
+    margin: 0 auto;
+  }
+  .swiper-wrapper {
+    display: flex;
+  }
+  .swiper-slide {
+    width: 480px !important;
+    @media (max-width: 1280px) {
+      width: calc((100vw - 36px) / 3) !important;
     }
-
-    .img-wrapper {
-        display: flex;
-        justify-content: flex-end;
+    @media (max-width: 768px) {
+      width: 259px !important;
+    }
+    a {
+      text-decoration: none;
+    }
+    .rect {
+      width: 100%;
+      aspect-ratio: 48 / 20;
+      background-color: #fff;
+      border-radius: 8px;
+      display: flex;
+      align-items: center;
+    }
+    .logo-rect {
+      margin: 24px;
+      border-radius: 12px;
+      width: 31.25%;
+      aspect-ratio: 1 / 1;
+      overflow: hidden;
+      @media (max-width: 768px) {
+        width: 25%;
+        margin: 20px;
+      }
+      img {
         width: 100%;
         height: 100%;
-        box-sizing: border-box;
+        object-fit: cover;
+        border-radius: 12px;
+      }
     }
-
-    .img-hide {
-        @media (max-width: 1280px) {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-        }
-    }
-
-    .title {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        position: relative;
-
-        .name {
-            width: 30%;
-            color: #fff;
-            font-family: Pretendard;
-            font-size: 28px;
-            font-weight: 700;
-            word-break: keep-all;
-
-            @media (max-width: 1280px) {
-                width: 100%;
-                font-family: Pretendard;
-                font-size: 20px;
-                font-style: normal;
-                font-weight: 700;
-            }
-        }
-
-        @media (max-width: 1280px) {
-            min-width: 100px;
-            height: 100%;
-            flex-direction: column;
-            align-items: center;
-            & svg {
-                width: 5vw;
-                position: absolute;
-                bottom: 0;
-                left: 10px;
-            }
-        }
-    }
-
-    &:hover {
-        background: ${props => props.hoverBackColor}, #212224;
-
-        .hover-hide,
-        .img-hide {
-            opacity: 0;
-            width: 0;
-            height: 0;
-            margin-top: 0;
-        }
-        .hover-text {
-            display: block;
-            width: 70%;
-            color: #fff;
-            font-family: Pretendard;
-            font-size: 18px;
-            font-style: normal;
-            font-weight: 500;
-            line-height: 150%;
-        }
-
-        @media (max-width: 1280px) {
-            flex-direction: column;
-            justify-content: flex-start;
-            .title {
-                margin-bottom: 24px;
-            }
-            .hover-text {
-                width: 100%;
-            }
-        }
-    }
-`;
-
-export const TrackWrapper = styled.div`
-    width: 100%;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-gap: 24px;
-
-    @media screen and (max-width: 767px) {
-        grid-template-columns: repeat(1, 1fr);
-    }
-`;
-
-export const TrackBox = styled.div<TrackBoxProps>`
-    width: 100%;
-    max-height: 312px;
-    height: auto;
-    display: flex;
-    justify-content: space-between;
-    background: #212224;
-    border-radius: 8px;
-    padding: 16px;
-    box-sizing: border-box;
-
-    .hover-text {
-        opacity: 0;
-        font-size: 0;
-    }
-
-    .img-wrapper {
-        display: flex;
-        justify-content: flex-end;
-        height: 100%;
-        box-sizing: border-box;
-    }
-
-    .img-hide {
-        @media (max-width: 1280px) {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-        }
-    }
-
-    .title {
-        position: relative;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        align-items: center;
-
-        .name {
-            width: 100%;
-            display: flex;
-            justify-content: start;
-            color: #fff;
-            font-family: Pretendard;
-            font-size: 28px;
-            font-weight: 700;
-
-            @media (max-width: 1280px) {
-                font-size: 20px;
-            }
-        }
-
-        @media (max-width: 1280px) {
-            min-width: 100px;
-            height: 100%;
-            flex-direction: column;
-            align-items: center;
-
-            & svg {
-                width: 5vw;
-                position: absolute;
-                bottom: 0;
-                left: 10px;
-            }
-        }
-    }
-
-    &:hover {
-        .title {
-        }
-
-        .name {
-            color: ${props => props.hoverColor};
-            @media (max-width: 768px) {
-                font-size: 15px;
-            }
-        }
-        .hover-hide {
-            opacity: 0;
-            width: 0;
-        }
-
-        .img-wrapper {
-            display: none;
-        }
-
-        .hover-text {
-            opacity: 100;
-            color: var(--Grey-200, #f2f4f6);
-            font-family: Pretendard;
-            font-size: 18px;
-            font-style: normal;
-            font-weight: 500;
-            line-height: 150%;
-            @media (max-width: 768px) {
-                font-size: 12px;
-                margin: 0 24px;
-            }
-        }
-        .img-hide {
-            opacity: 0;
-        }
-    }
-`;
-
-export const PlanBox = styled.div`
-    height: 195px;
-
-    @media (max-width: 500px) {
-        height: 130px;
-    }
-
-    .img {
-        width: 100%;
-        @media (max-width: 768px) {
-            width: 100%;
-        }
-    }
-    .week {
-        color: var(--Grey-900, #212224);
-        font-family: Pretendard;
-        font-size: 20px;
-        font-style: normal;
-        font-weight: 700;
-        line-height: 150%;
-        margin: 23.5px 0 7.5px 0;
-        @media (max-width: 768px) {
-            font-size: 3vw;
-            margin: 15px 0 5px 0;
-        }
-    }
-    .content {
-        color: var(--Grey-900, #212224);
-        font-family: Pretendard;
-        font-size: 20px;
-        font-style: normal;
+    .text-section {
+      font-family: Pretendard;
+      color: #212224;
+      .name {
+        color: #212224;
+        font-size: 28px;
         font-weight: 600;
-        line-height: 150%;
-        @media (max-width: 768px) {
-            font-size: 3vw;
-        }
-    }
-`;
-
-export const SupportBox = styled.div`
-    width: 100%;
-    gap: 24px;
-    display: flex;
-    margin-bottom: 24px;
-
-    .wrapper {
-        width: 100%;
-        background: var(--White, #fff);
-        border-radius: 8px;
-        padding: 28px 78px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        .logo {
-            height: 64px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-
-            @media (max-width: 1280px) {
-                height: 35px;
-            }
-        }
-
+        line-height: 200%;
         @media (max-width: 1280px) {
-            padding: 15px 35px;
+          font-size: 2.2vw;
         }
-    }
-`;
+        @media (max-width: 768px) {
+          font-size: 20px;
+        }
+      }
+      .gen {
+        font-size: 20px;
+        font-weight: 400;
+        @media (max-width: 1280px) {
+         
+        }
+        `;
