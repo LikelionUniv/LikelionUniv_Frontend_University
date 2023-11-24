ba7cc0c0e4e5ba49112783f310fa8dc390e2de17
byeong <qudcks4885@gmail.com>
2023-11-24 23:15:27 +0900

    feat : 로그인 상태 관리

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,8 +2,24 @@ import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 import Nav from '../components/Nav';
 import Footer from '../components/layout/Footer';
+import { useEffect } from 'react';
+import { requestUserInfo } from '../api/auth/auth';
+import { useRecoilState } from 'recoil';
+import { userState } from '../store/user';
 
-function root() {
+
+function Root() {
+    const [userinfo,updateUserinfo] = useRecoilState(userState);
+
+    useEffect(()=>{
+        const fetchUser =async () => {
+            const userInfo = await requestUserInfo();
+            console.log(userInfo);
+            updateUserinfo(userInfo);
+        }
+        fetchUser();
+    },[])
+
     return (
         <>
             <Nav />
@@ -16,7 +32,7 @@ function root() {
     );
 }
 
-export default root;
+export default Root;
 
 const Padding = styled.div`
     height: 56px;
