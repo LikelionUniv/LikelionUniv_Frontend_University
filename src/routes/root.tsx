dcc94d522ed83f44831afb5ef939f7e85da0c75d
byeong <qudcks4885@gmail.com>
2023-11-24 23:42:27 +0900

    Revert "feat : 로그인 상태 관리" This reverts commit ba7cc0c0e4e5ba49112783f310fa8dc390e2de17.

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,24 +2,8 @@ import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 import Nav from '../components/Nav';
 import Footer from '../components/layout/Footer';
-import { useEffect } from 'react';
-import { requestUserInfo } from '../api/auth/auth';
-import { useRecoilState } from 'recoil';
-import { userState } from '../store/user';
 
-
-function Root() {
-    const [userinfo,updateUserinfo] = useRecoilState(userState);
-
-    useEffect(()=>{
-        const fetchUser =async () => {
-            const userInfo = await requestUserInfo();
-            console.log(userInfo);
-            updateUserinfo(userInfo);
-        }
-        fetchUser();
-    },[])
-
+function root() {
     return (
         <>
             <Nav />
@@ -32,7 +16,7 @@ function Root() {
     );
 }
 
-export default Root;
+export default root;
 
 const Padding = styled.div`
     height: 56px;
