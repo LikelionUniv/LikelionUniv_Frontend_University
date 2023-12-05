0cf1cc6a782f9eef352f1efee027be8a1f53045c
김진호 <rlawlsgh1227@gmail.com>
2023-12-05 11:37:00 +0900

    fix: 토큰 재발급 부분 일단 제거

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,4 @@
 import axios from 'axios';
-import { jwtDecode } from 'jwt-decode';
-import reissue from './reissue';
 
 export const axiosInstance = axios.create({
     baseURL: 'https://stag.likelionuniv.com',
@@ -22,22 +20,22 @@ axiosInstance.interceptors.request.use(async config => {
         config.headers.Authorization = `Bearer ${token}`;
     }
 
-    if (token !== undefined) {
-        // access token 만료 검증
-        const expiredAt = jwtDecode(token).exp as number;
-        const now = Math.floor(Date.now() / 1000);
+    // if (token !== undefined) {
+    //     // access token 만료 검증
+    //     const expiredAt = jwtDecode(token).exp as number;
+    //     const now = Math.floor(Date.now() / 1000);
 
-        // access token이 만료됐을 때
-        if (expiredAt < now) {        
-            const reissueToken = await reissue();
-            if (reissueToken === undefined) throw new Error('에러');
+    //     // access token이 만료됐을 때
+    //     if (expiredAt < now) {        
+    //         const reissueToken = await reissue();
+    //         if (reissueToken === undefined) throw new Error('에러');
 
-            localStorage.setItem('access_token', reissueToken.accessToken);
-            localStorage.setItem('refresh_token', reissueToken.refreshToken);
+    //         localStorage.setItem('access_token', reissueToken.accessToken);
+    //         localStorage.setItem('refresh_token', reissueToken.refreshToken);
 
-            config.headers.Authorization = `Bearer ${reissueToken.accessToken}`;
-        }
-    }
+    //         config.headers.Authorization = `Bearer ${reissueToken.accessToken}`;
+    //     }
+    // }
 
     return config;
 });
