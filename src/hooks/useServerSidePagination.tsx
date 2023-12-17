6768d236a5df3eb9148db2dc3d4fe4fdc7ed14dd
김진호 <rlawlsgh1227@gmail.com>
2023-12-17 23:51:40 +0900

    fix: 연간 기부금모금액 다시 살림

--- src/hooks/useServerSidePagination.tsx
+++ src/hooks/useServerSidePagination.tsx
@@ -40,7 +40,7 @@ function useServerSidePagination<T>({
     search,
 }: IuseServerSidePagination): ReturnuseServerSidePagination<T> {
     const [data, setData] = useState<T[]>([]);
-    const [, setPageLength] = useState<number>(0);
+    const [pageLength, setPageLength] = useState<number>(0);
     const [loading, setLoading] = useState<boolean>(false);
     const [currentPage, setCurrentPage] = useState<number>(1);
 
@@ -83,7 +83,7 @@ function useServerSidePagination<T>({
     const renderPaginationBtn = (): JSX.Element => {
         return (
             <Pagination
-                totalPageNum={size}
+                totalPageNum={pageLength}
                 pageNum={currentPage}
                 setPageNum={setCurrentPage}
             />
