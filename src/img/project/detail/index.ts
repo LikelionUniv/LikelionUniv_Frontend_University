e3cb12da9ab08163bfd32bd0194e839d762861cf
zizonyoungjun <naya700@naver.com>
2023-12-17 22:32:02 +0900

    Design: icon 추가

--- src/img/project/detail/index.ts
+++ src/img/project/detail/index.ts
@@ -3,4 +3,20 @@ export { default as RightArrow } from './icon-rightArrow.svg';
 export { default as SwiperLeft } from './icon-swiperLeft.svg';
 export { default as SwiperRight } from './icon-swiperRight.svg';
 export { default as PopModal } from './icon-PopModal.svg';
-//export { default as ProjectImg } from './project-detail.png';
+
+export { default as ModalRightOn } from './icon-modalRightOn.svg';
+export { default as ModalLeftOff } from './icon-modalLeftOff.svg';
+export { default as ModalRightOff } from './icon-modalRightOff.svg';
+export { default as ModalLeftOn } from './icon-modalLeftOn.svg';
+
+export { default as TransLeftOff } from './icon-transparentLeftOff.svg';
+export { default as TransLeftOn } from './icon-transparentLeftOn.svg';
+export { default as TransRightOff } from './icon-transparentRightOff.svg';
+export { default as TransRightOn } from './icon-transparentRightOn.svg';
+
+export { default as Chevron_Left } from './Chevron_Left.svg';
+export { default as Chevron_Right } from './Chevron_Right.svg';
+
+
+export { default as Close } from './icon-close.svg';
+
