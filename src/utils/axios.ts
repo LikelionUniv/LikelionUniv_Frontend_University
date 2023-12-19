753931915e411518fe65b96f458e7dc50e472855
김진호 <rlawlsgh1227@gmail.com>
2023-12-20 00:53:14 +0900

    fix: try catch 대신 axios의 response.use로 에러처리 변경

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -23,15 +23,26 @@ axiosInstance.interceptors.response.use(
     response => {
         return response;
     },
-    async error => {
-        // const axiosError = getAxiosError(error);
+    error => {
+        const axiosError = getAxiosError(error);
+        // const {config} = error; 
+
+        // // 리프레시도 만료된 경우나 잘못된 토큰인 경우
+        // if (axiosError?.code === 'TOKEN_401_1') {
+        //     return Promise.reject(error);
+        // }
 
         // // 액세스 토큰 만료
-        // if (axiosError?.code === 'TOKEN_401_1') {
+        // if (axiosError?.code === 'TOKEN_401_4') {
+        //     const originRequest = config;
         //     const reissueToken = await reissue();
-        //     console.log(reissueToken);
-        //     return axiosInstance.request(error.config);
+
+        //     setAccessAndRefresh(reissueToken.accessToken, reissueToken.refreshToken);
+        //     originRequest.headers.Authorization = `Bearer ${reissueToken.accessToken}`;
+
+        //     return axiosInstance(originRequest);
         // }
+        alert(axiosError?.message);
         return Promise.reject(error);
     },
 );
@@ -40,3 +51,8 @@ const getAxiosError = (error: AxiosError): IError | undefined => {
     const serverError = error as AxiosError<IError>;
     return serverError.response?.data;
 };
+
+// const setAccessAndRefresh = (accessToken: string, refreshToken: string) => {
+//     localStorage.setItem('access_token', accessToken);
+//     localStorage.setItem('refresh_token', refreshToken);
+// }
