16b508a61c2a1a89358314498adee45956c9f8e4
byeong <qudcks4885@gmail.com>
2023-12-28 22:50:59 +0900

    fix: useAuth 유저 role 추가

--- src/store/user.ts
+++ src/store/user.ts
@@ -4,14 +4,17 @@ type UserInfo = {
     name: string;
     profileImage: string;
     userId: number;
+    role: string,
     isLogin: boolean;
 };
+
 export const userState = atom<UserInfo>({
     key: 'userInfo',
     default: {
         name: '',
         profileImage: '',
         userId: -1,
+        role:'',
         isLogin: false,
     },
 });
