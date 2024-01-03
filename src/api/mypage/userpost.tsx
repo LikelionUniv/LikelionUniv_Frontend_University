80c93c174826ebdc73276983e3a439487816bd20
Ariling <97501226+Ariling@users.noreply.github.com>
2024-01-03 20:36:44 +0900

    fix : api 수정

--- src/api/mypage/userpost.tsx
+++ src/api/mypage/userpost.tsx
@@ -1,47 +1,36 @@
 import axios from 'axios';
+import { axiosInstance } from '../../utils/axios';
 
 export const mypageGetPostApi = async (user_id: number, page: number) => {
-    const token = localStorage.getItem('access-token');
-    try {
-        const response = await axios.get(
-            `${process.env.REACT_APP_BASE_URL}/api/v1/user/${user_id}/posts?page=${page}`,
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                    withCredentials: true,
-                },
-            },
-        );
-        console.log(response.data.data);
-        return response.data.data;
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            const result = error.response?.data?.detail;
-            return result;
-        }
-    }
+    return await axiosInstance
+        .get(
+            `${process.env.REACT_APP_BASE_URL}/v1/user/${user_id}/posts?page=${page}`,
+        )
+        .then(response => {
+            return response.data.data;
+        })
+        .catch(error => {
+            if (axios.isAxiosError(error)) {
+                const result = error.response?.data?.detail;
+                return result;
+            }
+        });
 };
 
 export const mypageGetCommentApi = async (user_id: number, page: number) => {
-    const token = localStorage.getItem('access-token');
-    try {
-        const response = await axios.get(
-            `${process.env.REACT_APP_BASE_URL}/api/v1/user/${user_id}/posts/comment?page=${page}`,
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                    withCredentials: true,
-                },
-            },
-        );
-        console.log(response.data.data);
-        return response.data.data;
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            const result = error.response?.data?.detail;
-            return result;
-        }
-    }
+    return await axiosInstance
+        .get(
+            `${process.env.REACT_APP_BASE_URL}/v1/user/${user_id}/posts/comment?page=${page}`,
+        )
+        .then(response => {
+            return response.data.data;
+        })
+        .catch(error => {
+            if (axios.isAxiosError(error)) {
+                const result = error.response?.data?.detail;
+                return result;
+            }
+        });
 };
 
 export const myPageGetLikeApi = async (
@@ -50,49 +39,38 @@ export const myPageGetLikeApi = async (
     sort?: string,
     search?: string,
 ) => {
-    const token = localStorage.getItem('access-token');
-    try {
-        let url = `${process.env.REACT_APP_BASE_URL}/api/v1/user/${user_id}/posts/like?page=${page}`;
-        if (sort) {
-            url += `&sort=${sort}`;
-        }
-        if (search) {
-            url += `&search=${search}`;
-        }
-        const response = await axios.get(url, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-                withCredentials: true,
-            },
-        });
-        console.log(response.data.data);
-        return response.data.data;
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            const result = error.response?.data?.detail;
-            return result;
-        }
+    let url = `${process.env.REACT_APP_BASE_URL}/v1/user/${user_id}/posts/like?page=${page}`;
+    if (sort) {
+        url += `&sort=${sort}`;
     }
+    if (search) {
+        url += `&search=${search}`;
+    }
+    return await axiosInstance
+        .get(url)
+        .then(response => {
+            return response.data.data;
+        })
+        .catch(error => {
+            if (axios.isAxiosError(error)) {
+                const result = error.response?.data?.detail;
+                return result;
+            }
+        });
 };
 
 export const mypageGetProjectApi = async (user_id: number, page: number) => {
-    const token = localStorage.getItem('access-token');
-    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    try {
-        const response = await axios.get(
-            `${process.env.REACT_APP_BASE_URL}/api/v1/user/${user_id}/projects?page=${page}`,
-            {
-                headers: {
-                    withCredentials: true,
-                },
-            },
-        );
-        console.log(response.data.data);
-        return response.data.data;
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            const result = error.response?.data?.detail;
-            return result;
-        }
-    }
+    return await axiosInstance
+        .get(
+            `${process.env.REACT_APP_BASE_URL}/v1/user/${user_id}/projects?page=${page}`,
+        )
+        .then(response => {
+            return response.data.data;
+        })
+        .catch(error => {
+            if (axios.isAxiosError(error)) {
+                const result = error.response?.data?.detail;
+                return result;
+            }
+        });
 };
