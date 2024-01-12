bf08ff91cca5add273fd8038326d9d8e6346ea8e
caminobelllo <alstpgml0812@gmail.com>
2024-01-12 16:11:13 +0900

    feat : 채팅 페이지 연결

--- src/routes/Chat.tsx
+++ src/routes/Chat.tsx
@@ -1,7 +1,9 @@
+import ChatHome from '../components/chatting/ChatHome';
+
 const Chat = () => {
     return (
         <div className="pDiv">
-            <h1>채팅</h1>
+            <ChatHome />
         </div>
     );
 };
