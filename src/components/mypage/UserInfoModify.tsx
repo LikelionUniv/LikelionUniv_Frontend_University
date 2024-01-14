121a0117c481913fb633d4be960a7e1e958ed23d
byeong <qudcks4885@gmail.com>
2024-01-14 18:06:29 +0900

    fix: 내정보수정 트랙 정보 추가, 드롭다운 - 내 파트 정보 보이도록 수정

--- src/components/mypage/UserInfoModify.tsx
+++ src/components/mypage/UserInfoModify.tsx
@@ -14,9 +14,11 @@ import {
 
 /* dropdown option 부분 */
 const trackOptions = [
-    { value: 1, label: '기획디자인' },
-    { value: 2, label: '프론트엔드' },
-    { value: 3, label: '백엔드' },
+    { value: 1, label: '기획' },
+    { value: 2, label: '디자인' },
+    { value: 3, label: '기획디자인' },
+    { value: 4, label: '백엔드' },
+    { value: 5, label: '프론트엔드' },
 ];
 
 function findLabelByValue(value: number) {
@@ -64,6 +66,7 @@ const UserInfoModify = () => {
                 let label: string;
                 if (field === 'part') {
                     label = findLabelByValue(selectedOption.value);
+                    console.log(label)
                 }
                 setFormState(prev => ({
                     ...prev,
@@ -170,6 +173,7 @@ const UserInfoModify = () => {
                             <DropDown
                                 options={trackOptions}
                                 onChange={handleSelectChange('part')}
+                                placeholder={convertRole(formState.part)}
                             />
                         </div>
                     </div>
