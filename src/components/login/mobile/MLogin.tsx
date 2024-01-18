0d63b017c1a68a894dffabf0f8d569737a2b36b5
byeong <qudcks4885@gmail.com>
2024-01-18 15:51:43 +0900

    design: 로그인 화면 모바일 , 웹 버전 디자인 수정

--- src/components/login/mobile/MLogin.tsx
+++ src/components/login/mobile/MLogin.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import LoginImage from '../../../img/login/Login.png';
 import * as ML from './MLogin.style';
 import MMoreInfo from './MMoreInfo';
-import MLoginComplete from './MLoginComplete';
 
-const MLogin = () => {
+interface LoginProps {
+    google_login: () => void,
+    kakao_login: () => void
+  }
+
+const MLogin = ({google_login, kakao_login}:LoginProps) => {
     return (
         <>
             <ML.Container>
@@ -16,16 +20,14 @@ const MLogin = () => {
                 <ML.Item src={LoginImage} alt="login" />
 
                 <ML.Box>
-                    <ML.GoogleBtn>
+                    <ML.GoogleBtn onClick={google_login}>
                         <span>Google 계정으로 로그인</span>
                     </ML.GoogleBtn>
-                    <ML.KakaoBtn>
+                    <ML.KakaoBtn onClick={kakao_login}>
                         <span>카카오 로그인</span>
                     </ML.KakaoBtn>
                 </ML.Box>
             </ML.Container>
-            <MMoreInfo />
-            <MLoginComplete />
         </>
     );
 };
