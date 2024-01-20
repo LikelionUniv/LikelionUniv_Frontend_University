d59ffd47421a70083f0ca5e579fd6d9236105bdd
byeong <qudcks4885@gmail.com>
2024-01-20 17:53:48 +0900

    feat: global State에 유저 정보 저장
    - Atom userState를 사용하는 컴포넌트가 있어서 같이 업데이트하도록 두었습니다.
    *후에 삭제 예정

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,7 +2,29 @@ import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 import { requestUserInfo } from '../api/auth/auth';
 
+import { userState } from '../store/user';
+import { useSetRecoilState } from 'recoil';
+import { useEffect } from 'react';
+
+type User = {
+    name: string;
+    profileImage: string;
+    userId: number;
+    role: string;
+    isLogin: boolean;
+};
+
+const fallback = {
+    name: '',
+    profileImage: '',
+    userId: -1,
+    role: '',
+    isLogin: false,
+};
+
 export const useAuth = () => {
+    const updateUserState = useSetRecoilState(userState);
+
     const access_token = localStorage.getItem('access_token');
     const queryClient = useQueryClient();
 
@@ -12,14 +34,17 @@ export const useAuth = () => {
     };
 
     const {
-        data: userinfo,
+        data: userinfo = fallback,
         isLoading,
         error,
-    } = useQuery({
+    } = useQuery<User>({
         queryKey: ['login-user-information'],
         queryFn: fetchUser,
         enabled: !!access_token,
     });
+    useEffect(() => {
+        updateUserState(userinfo);
+    }, [userinfo, updateUserState]);
 
     const handleLogout = () => {
         localStorage.clear();
