cbab0367991d2fa5ff983de198615248f1c87327
KimKyuHoi <k546kh@gmail.com>
2024-06-26 17:19:04 +0900

    feat: 랜딩 페이지 전체 구조 세팅

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,9 +1,11 @@
 //import '../components/LoadScript';
 
-import Landing from './components/Landing';
+// import Landing from './components/Landing';
+import HakathonLanding from './components/hakathon/HakathonLanding';
 
 const LandingPage = () => {
-    return <Landing />;
+    // return <Landing />;
+    return <HakathonLanding />;
 };
 
 export default LandingPage;
