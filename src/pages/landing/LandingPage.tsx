efb9029b72b3bb7adb8a11ada3421ddfadb304ba
KimKyuHoi <k546kh@gmail.com>
2024-06-26 17:20:03 +0900

    Comment: 랜딩 페이지 주석달기

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -4,7 +4,7 @@
 import HakathonLanding from './components/hakathon/HakathonLanding';
 
 const LandingPage = () => {
-    // return <Landing />;
+    // return <Landing />; // 추후 랜딩 페이지에 관련해서 해커톤 기간이 끝나면 다시 되돌릴 계획입니다.
     return <HakathonLanding />;
 };
 
