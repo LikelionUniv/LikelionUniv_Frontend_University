baac01951cdab2d0e40a84486372920fd8417d9c
김규회 <48755156+KimKyuHoi@users.noreply.github.com>
2024-06-26 17:50:18 +0900

    해커톤 랜딩 페이지 기본 구조도 세팅  (#76)
    * Design: 공용 디자인 CSS 구현
    
    * feat: 랜딩 페이지 전체 구조 세팅
    
    * Comment: 랜딩 페이지 주석달기

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,9 +1,11 @@
 //import '../components/LoadScript';
 
-import Landing from './components/Landing';
+// import Landing from './components/Landing';
+import HakathonLanding from './components/hakathon/HakathonLanding';
 
 const LandingPage = () => {
-    return <Landing />;
+    // return <Landing />; // 추후 랜딩 페이지에 관련해서 해커톤 기간이 끝나면 다시 되돌릴 계획입니다.
+    return <HakathonLanding />;
 };
 
 export default LandingPage;
---
+++ src/pages/landing/components/hakathon/HakathonLanding.tsx
@@ -0,0 +1,11 @@
+import { Layout, Container } from '../../../../styles/Layout';
+
+const HakathonLanding = () => {
+    return (
+        <Layout>
+            <Container>{/*여기서 이제 컴포넌트 작성하면 됩니다.*/}</Container>
+        </Layout>
+    );
+};
+
+export default HakathonLanding;
