dae85c93adeaf84932fa8fd0a2e4d84c83d1e335
KimKyuHoi <k546kh@gmail.com>
2024-06-27 14:09:39 +0900

    feat: 공용컴포넌트 구현

---
+++ src/components/text/Typography.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from 'react';
+
+import styled from 'styled-components';
+
+interface ITextContentProps {
+    children?: ReactNode;
+    size?: string;
+    weight?: string;
+    color?: string;
+    margin?: string;
+}
+
+const StyledText = styled.div<ITextContentProps>`
+    font-size: ${({ size }) => size || '1rem'};
+    font-weight: ${({ weight }) => weight || 'normal'};
+    color: ${({ color }) => color || 'inherit'};
+    margin: ${({ margin }) => margin || '0'};
+`;
+
+const Typography = ({
+    children,
+    size,
+    weight,
+    color,
+    margin,
+    ...rest
+}: ITextContentProps) => {
+    return (
+        <StyledText
+            size={size}
+            weight={weight}
+            color={color}
+            margin={margin}
+            {...rest}
+        >
+            {children}
+        </StyledText>
+    );
+};
+
+export default Typography;
