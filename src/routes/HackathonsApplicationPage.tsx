3f28f06ff9646214a5d35328b26c47601aa8120f
ivoryeee <psa109@sookmyung.ac.kr>
2024-06-28 20:58:32 +0900

    Feat: 로그인 시에만 신청 폼 작성 가능

--- src/routes/HackathonsApplicationPage.tsx
+++ src/routes/HackathonsApplicationPage.tsx
@@ -1,6 +1,26 @@
 import ApplicationForm from '../components/application/ApplicationForm';
+import { useEffect } from 'react';
+import { useAuth } from '../hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
 
 const HackathonsApplicationPage = () => {
+    const { userinfo, isLoading } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (isLoading) {
+            return;
+        }
+
+        if (!userinfo?.isLogin) {
+            alert('로그인 후 이용 기능합니다.');
+            navigate('/login');
+        } else if (userinfo?.role === 'GUEST') {
+            alert('작성 권한이 없는 회원입니다.');
+            navigate('/');
+        }
+    }, [userinfo, navigate, isLoading]);
+
     return (
         <>
             <ApplicationForm />
