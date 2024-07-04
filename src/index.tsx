dd1e33ec25c364e46c5a67c8e6c94ee40a6affca
DevTeddy123 <ab315601@naver.com>
2024-07-05 01:46:52 +0900

    Feat : google analytics 적용

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ import router from './Router';
 import './styles/font.css';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import ReactGA from 'react-ga';
 
 // stale time 10분으로 설정
 const queryClient = new QueryClient({
@@ -20,14 +19,6 @@ const queryClient = new QueryClient({
         },
     },
 });
-
-//Google Analytics
-// const gaTrackingId = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID; // 환경 변수에 저장된 추적ID 가져오기
-
-// if (gaTrackingId) {
-//     ReactGA.initialize(gaTrackingId, { debug: true });
-//     ReactGA.pageview(window.location.pathname);
-// }
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
