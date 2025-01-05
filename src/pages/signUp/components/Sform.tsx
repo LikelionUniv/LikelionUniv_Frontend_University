415174704adc35e7f881d852d011b02d3735159c
DevTeddy123 <ab315601@naver.com>
2025-01-05 12:33:05 +0900

    Feat : SignUp 기수 추가

--- src/pages/signUp/components/Sform.tsx
+++ src/pages/signUp/components/Sform.tsx
@@ -3,7 +3,7 @@ import './signUp.css';
 import SchoolDropDown from './SchoolDropDown';
 import { useState } from 'react';
 import { ActionMeta } from 'react-select';
-import { OptionType } from './DropDown';
+import DropDownOrdinal, { OptionType } from './DropDownOrdinal';
 import { useParams } from 'react-router-dom';
 import { axiosInstance } from '../../../api/axios';
 import { LoginComplete } from '../../login/components/LoginComplete';
@@ -47,9 +47,20 @@ for (let i = 11; i >= 1; i--) {
 }
 
 const trackOptions = [
-    { value: 1, label: '기획디자인' },
-    { value: 2, label: '프론트엔드' },
-    { value: 3, label: '백엔드' },
+    { value: 0, label: '알럼나이' },
+    { value: 1, label: '1기' },
+    { value: 2, label: '2기' },
+    { value: 3, label: '3기' },
+    { value: 4, label: '4기' },
+    { value: 5, label: '5기' },
+    { value: 6, label: '6기' },
+    { value: 7, label: '7기' },
+    { value: 8, label: '8기' },
+    { value: 9, label: '9기' },
+    { value: 10, label: '10기' },
+    { value: 11, label: '11기' },
+    { value: 12, label: '12기' },
+    { value: 13, label: '13기' },
 ];
 
 const roleOptions = [
@@ -131,6 +142,8 @@ const Sform = () => {
         }
     };
 
+    const handleOrdinal = () => {};
+
     return (
         <>
             {!isSuccess ? (
@@ -159,6 +172,15 @@ const Sform = () => {
                             })
                         }
                     />
+
+                    <Ndiv>기수 선택</Ndiv>
+                    {/* <Wapper> */}
+                    <DropDownOrdinal
+                        options={trackOptions}
+                        onChange={handleOrdinal}
+                        placeholder={'기수를 선택해주세요.'}
+                    />
+                    {/* </Wapper> */}
                     <button className="saveBtn" onClick={handleSubmit}>
                         저장하기
                     </button>
@@ -170,4 +192,10 @@ const Sform = () => {
     );
 };
 
+const Wapper = styled.div`
+    background-color: red;
+    & > div {
+        width: 100%;
+    }
+`;
 export default Sform;
--- src/pages/signUp/components/customSelectStyles.tsx
+++ src/pages/signUp/components/customSelectStyles.tsx
@@ -19,7 +19,7 @@ const roleTrackStyle = {
         border: state.isFocused
             ? ' 1px solid #ff7710'
             : '1px solid var(--grey-400, #DCDFE3)',
-        width: state.isFocused ? '138px' : '138px',
+        // width: state.isFocused ? '138px' : '138px',
         height: state.isFocused ? '48px' : '48px',
         borderRadius: '6px',
         backgroundColor: 'white',
@@ -210,4 +210,140 @@ const schoolStyle = {
     }),
 };
 
-export { roleTrackStyle, genStyle, schoolStyle };
+// 알럼나이 select 스타일
+const ordinalRoleTrackStyle = {
+    indicatorSeparator: () => ({
+        backgroundColor: 'transparent',
+    }),
+    valueContainer: () => ({
+        display: 'flex',
+        paddingLeft: '10px',
+    }),
+    placeholder: () => ({
+        color: 'var(--grey-600, #adb3ba)',
+    }),
+    control: (provided: any, state: any) => ({
+        ...provided,
+        fontFamily: 'Pretendard',
+        fontSize: '16px',
+        fontWeight: '500',
+        paddingLeft: '6px',
+        border: state.isFocused
+            ? ' 1px solid #ff7710'
+            : '1px solid var(--grey-400, #DCDFE3)',
+        height: state.isFocused ? '48px' : '48px',
+        borderRadius: '6px',
+        backgroundColor: 'white',
+        boxShadow: '0 0 0 0px transparent',
+        '&:hover': {
+            borderColor: '#ff7710',
+            border: '1px solid #ff7710',
+        },
+    }),
+    menu: (provided: any) => ({
+        ...provided,
+        borderRadius: '6px',
+        boxShadow: '0 0 0 0px transparent',
+        border: '1px solid var(--grey-400, #DCDFE3)',
+        backgroundColor: 'white',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    }),
+    menuList: (provided: any) => ({
+        ...provided,
+        backgroundColor: 'transparent',
+        maxHeight: '136px', // gap을 반영한 스크롤 없는 최소 높이
+        width: '138px', // 너비 조정
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '4px',
+    }),
+    option: (provided: any, state: any) => ({
+        ...provided,
+        padding: '4px',
+        paddingLeft: '12px',
+        fontFamily: 'Pretendard',
+        fontSize: '16px',
+        fontWeight: '500',
+        width: '128px',
+        height: '40px',
+        color: 'var(--grey-900, #212224)',
+        borderRadius: '4px',
+        display: 'flex',
+        alignItems: 'center',
+        flexShrink: 0,
+        backgroundColor: state.isFocused
+            ? 'var(--grey-300, #EAECEE)'
+            : state.isSelected
+            ? 'var(--grey-300, #EAECEE)'
+            : provided.backgroundColor,
+        '&:active': {
+            backgroundColor: 'var(--grey-400, #DCDFE3)',
+        },
+    }),
+};
+
+// 알럼나이 select 스타일
+const ordinalStyle = {
+    ...ordinalRoleTrackStyle,
+    menu: (provided: any) => ({
+        ...provided,
+        borderRadius: '6px',
+        boxShadow: '0 0 0 0px transparent',
+        border: '1px solid var(--grey-400, #DCDFE3)',
+        backgroundColor: 'white',
+        display: 'flex',
+        flexDirection: 'column',
+    }),
+    menuList: (provided: any) => ({
+        ...provided,
+        backgroundColor: 'transparent',
+        maxHeight: '136px',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '4px',
+        paddingLeft: '4px',
+        '::-webkit-scrollbar': {
+            width: '4px',
+        },
+        '::-webkit-scrollbar-track': {
+            margin: '4px 0',
+            borderRadius: '3px',
+        },
+        '::-webkit-scrollbar-thumb': {
+            background: '#D1D4D8',
+            borderRadius: '3px',
+        },
+        '::-webkit-scrollbar-thumb:hover': {
+            background: '#D1D4D8',
+        },
+    }),
+    option: (provided: any, state: any) => ({
+        ...provided,
+        padding: '4px',
+        paddingLeft: '12px',
+        fontFamily: 'Pretendard',
+        fontSize: '16px',
+        fontWeight: '500',
+        width: '100%',
+        height: '40px',
+        color: 'var(--grey-900, #212224)',
+        borderRadius: '4px',
+        display: 'flex',
+        alignItems: 'center',
+        flexShrink: 0,
+        backgroundColor: state.isFocused
+            ? 'var(--grey-300, #EAECEE)'
+            : state.isSelected
+            ? 'var(--grey-300, #EAECEE)'
+            : provided.backgroundColor,
+        '&:active': {
+            backgroundColor: 'var(--grey-400, #DCDFE3)',
+        },
+    }),
+};
+
+export { roleTrackStyle, genStyle, schoolStyle, ordinalStyle };
